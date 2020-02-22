2cf746b4ae346ebe3061e98a73818b6f01e6a6b0
cutefluffyfox <mishkazel1@gmail.com>
2020-02-22 17:26:04 +0300

    added: encapsulation, methods for homework and classwork

--- script/map_api.py
+++ script/map_api.py
@@ -1,5 +1,4 @@
 from io import BytesIO
-
 import requests
 
 
@@ -8,69 +7,113 @@ class MapAPI:
     geocode_token = "40d1649f-0493-4b70-98ba-98533de7710b"
     maps_api_server = "http://static-maps.yandex.ru/1.x/"
 
-    def __init__(self, address: str):
-        self.address = address  # адрес, без него никак
-        self.json = self.get_json()  # все данные там, так что один раз читаем ради ускорения
-        self.object_json = self.get_object_json()  # важные данные там, так что один раз находим ради ускорения
-        self.cords = self.get_cords()  # они везде используются, б
-        self.zoom = "0"
-        self.spn = self.auto_spn()
-        self.style = "map"
-        self.pin = ""
-        self.image = None
-        self.update_image()
+    def __init__(self, address: str, map_size="650,450"):
+        self.address = ""
+        self.map_size = map_size
+        self.__json = dir()
+        self.__object_json = dict()
+        self.__cords = ""
+        self.__zoom = 2
+        self.__style = "map"
+        self.__pin = []
+        self.__image = None
+        self.set_address(address)
 
-    def get_json(self):
+    def set_address(self, address: str):
+        self.address = address
+        self.__json = self.__get_json(self.address)
+        self.__object_json = self.__get_object_json(self.__json)
+        self.__cords = self.__get_cords(self.__object_json)
+        self.__zoom = 2
+        self.__pin = []
+        self.__image = None
+        self.__update_image()
+
+    def set_map_size(self, map_size: str):
+        self.map_size = map_size
+
+    def __get_json(self, address: str):
         params = {
             "apikey": self.geocode_token,
-            "geocode": self.address,
+            "geocode": address,
             "format": "json"
         }
         return requests.get(self.geocode_server, params).json()
 
-    def get_object_json(self):
-        return self.json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
+    @staticmethod
+    def __get_object_json(json: dict):
+        return json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
 
-    def get_cords(self) -> str:
-        return self.object_json['Point']['pos'].replace(" ", ",")
+    @staticmethod
+    def __get_cords(object_json: dict) -> str:
+        return object_json['Point']['pos'].replace(" ", ",")
 
-    def update_image(self):
+    def __update_image(self):
         params = {
-            "ll": self.cords,
-            "l": self.style,
-            "pt": self.pin,
-            "z": self.zoom,
+            "ll": self.__cords,
+            "l": self.__style,
+            "pt": "~".join(self.__pin),
+            "z": self.__zoom,
+            "size": "650,450",
         }
         response = requests.get(self.maps_api_server, params=params)
         if response.status_code == 200:
-            self.image = BytesIO(response.content)
+            self.__image = BytesIO(response.content)
+
+    def add_pin(self, address: str, pin_style="pm2rdm"):
+        self.__pin.append(self.__get_cords(self.__get_object_json(self.__get_json(address))) + "," + pin_style)
 
     def get_image(self) -> BytesIO:
-        return self.image
+        return self.__image
 
-    def auto_spn(self) -> str:
-        lower = self.str_to_list(self.object_json['boundedBy']['Envelope']['lowerCorner'].replace(" ", ","))
-        upper = self.str_to_list(self.object_json['boundedBy']['Envelope']['upperCorner'].replace(" ", ","))
+    def __auto_spn(self) -> str:
+        lower = self.__str_to_list(self.__object_json['boundedBy']['Envelope']['lowerCorner'].replace(" ", ","))
+        upper = self.__str_to_list(self.__object_json['boundedBy']['Envelope']['upperCorner'].replace(" ", ","))
         return f"{abs(upper[0] - lower[0])},{abs(upper[1] - lower[1])}"
 
     def zoom_in(self, delta: int):
-        new_zoom = int(self.zoom) + delta
-        if 1 <= new_zoom <= 20:
-            self.zoom = str(new_zoom)
-            self.update_image()
+        self.set_zoom(int(self.__zoom) + delta)
 
     def set_zoom(self, new_zoom: int):
-        if 1 <= new_zoom <= 20:
-            self.zoom = str(new_zoom)
-            self.update_image()
+        if 2 <= new_zoom <= 18:
+            self.__zoom = new_zoom
+            self.__update_image()
 
-    def is_ok(self, spn=None, zoom=None, style="map", pins=None):
+    def __auto_change(self) -> float:
+        return 0.0006 * pow(2, 18 - self.__zoom)
+
+    def __move(self, x=0, y=0):
+        new_cords = self.__str_to_list(self.__cords)
+        new_cords = self.__list_to_str([new_cords[0] + x, new_cords[1] + y])
+        if self.__is_ok(cords=new_cords, zoom=self.__zoom, pin=self.__pin):
+            self.__cords = new_cords
+            self.__update_image()
+
+    def move_up(self, y=None):
+        self.__move(0, self.__auto_change() if y is None else -y)
+
+    def move_down(self, y=None):
+        self.__move(0, self.__auto_change() if y is None else y)
+
+    def move_right(self, x=None):
+        self.__move(self.__auto_change() if x is None else x, 0)
+
+    def move_left(self, x=None):
+        self.__move(self.__auto_change() if x is None else -x, 0)
+
+    def set_style(self, style: str):
+        if self.__is_ok(style=style):
+            self.__style = style
+            self.__update_image()
+
+    def __is_ok(self, cords=None, zoom=None, style="map", pin=None):
+        if cords is None:
+            cords = self.__cords
         params = {
-            "ll": self.cords,
+            "ll": cords,
             "l": style,
-            "pt": pins,
+            "pt": pin,
             "z": zoom,
-            "spn": spn,
         }
         response = requests.get(self.maps_api_server, params=params)
         return response.status_code == 200
@@ -80,22 +123,9 @@ class MapAPI:
         return ",pm2" + color + "m"
 
     @staticmethod
-    def str_to_list(data: str) -> [float, float]:
+    def __str_to_list(data: str) -> [float, float]:
         return list(map(float, data.split(",")))
 
     @staticmethod
-    def list_to_str(data: [float, float]) -> str:
+    def __list_to_str(data: [float, float]) -> str:
         return f"{data[0]},{data[1]}"
-
-
-def debug():
-    from PIL import Image
-    map_api = MapAPI("некрасовская 50")
-    map_api.set_zoom(15)
-    Image.open(map_api.get_image()).show()
-    map_api.set_zoom(18)
-    Image.open(map_api.get_image()).show()
-
-
-if __name__ == "__main__":
-    debug()
