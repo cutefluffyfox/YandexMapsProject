00dbc09e1e8ef2d1fe4d113eafac0524869aeafc
leffff <57654885+leffff@users.noreply.github.com>
2020-02-23 17:00:05 +1000

    Yandex lyceum api3 task completed

--- script/maps_api2.py
+++ script/maps_api2.py
@@ -15,38 +15,42 @@ class MapDisplay(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi(join(UI, "map_output.ui"), self)
-
         self.map_api = MapAPI("Океанский просп., 17, Владивосток")
         self.map_api.set_zoom(10)
         self.name = f"map_{str(len(listdir(RESULTS)) + 1)}.png"
+        self.update()
 
+    def update(self):
         img = Image.open(self.map_api.get_image())
         img.save(join(split(getcwd())[0], f"results/{self.name}"))
-
         scene = QGraphicsScene()
         scene.addPixmap(QPixmap(join(split(getcwd())[0], f"results/{self.name}")))
         self.display.setScene(scene)
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_W:
+        if event.key() == Qt.Key_W: #вверх
+            self.map_api.move_up()
+            self.update()
+
+        if event.key() == Qt.Key_S: #вниз
+            self.map_api.move_down()
+            self.update()
+
+        if event.key() == Qt.Key_A: #влево
+            self.map_api.move_left()
+            self.update()
+
+        if event.key() == Qt.Key_D: #вправо
+            self.map_api.move_right()
+            self.update()
+
+        if event.key() == Qt.Key_R: #приближение
             self.map_api.zoom_in(1)
+            self.update()
 
-            img = Image.open(self.map_api.get_image())
-            img.save(join(split(getcwd())[0], f"results/{self.name}"))
-
-            scene = QGraphicsScene()
-            scene.addPixmap(QPixmap(join(split(getcwd())[0], f"results/{self.name}")))
-            self.display.setScene(scene)
-
-        if event.key() == Qt.Key_S:
+        if event.key() == Qt.Key_F: #отдаление
             self.map_api.zoom_in(-1)
-
-            img = Image.open(self.map_api.get_image())
-            img.save(join(split(getcwd())[0], f"results/{self.name}"))
-
-            scene = QGraphicsScene()
-            scene.addPixmap(QPixmap(join(split(getcwd())[0], f"results/{self.name}")))
-            self.display.setScene(scene)
+            self.update()
 
 
 if __name__ == "__main__":
