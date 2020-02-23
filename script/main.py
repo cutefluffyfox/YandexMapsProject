ef195f255d9b207e32b08101c6d6caac14a8b735
leffff <57654885+leffff@users.noreply.github.com>
2020-02-23 15:31:40 +1000

    first map display program created

--- script/main.py
+++ script/main.py
@@ -1,1 +1,34 @@
-#  main file where all application word
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene
+import sys
+from os import getcwd, listdir
+from os.path import join, split
+from PyQt5.QtGui import QPixmap
+UI = join(split(getcwd())[0], "ui")
+from script.map_api import MapAPI
+from PIL import Image
+RESULTS = join(split(getcwd())[0], "results")
+
+
+class MapDisplay(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(join(UI, "map_output.ui"), self)
+
+        map_api = MapAPI("Океанский просп., 17, Владивосток")
+        map_api.set_zoom(18)
+        name = f"map_{str(len(listdir(RESULTS)) + 1)}.png"
+
+        img = Image.open(map_api.get_image())
+        img.save(join(split(getcwd())[0], f"results/{name}"))
+
+        scene = QGraphicsScene()
+        scene.addPixmap(QPixmap(join(split(getcwd())[0], f"results/{name}")))
+        self.display.setScene(scene)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = MapDisplay()
+    ex.show()
+    sys.exit(app.exec_())
